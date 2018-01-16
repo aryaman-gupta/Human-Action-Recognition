4f1951f01b7a094edeeb5b76869ed61c3050e4c0
Aryaman <aryaman1994@gmail.com>
2018-01-16 18:21:11 +0530

    Wrote distPoints and rearrangePoints functions

--- distPoints.m
+++ distPoints.m
@@ -1,7 +1,9 @@
 function [ dist ] = distPoints( point1, point2 )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+%Calculates distance between two 3D points
 
-
+dist = (point1(1) - point2(1))*(point1(1) - point2(1));
+dist = dist + (point1(2) - point2(2))*(point1(2) - point2(2));
+dist = dist + (point1(3) - point2(3))*(point1(3) - point2(3));
+dist = sqrt(dist);
 end
 
